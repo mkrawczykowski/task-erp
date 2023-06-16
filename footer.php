4f8f175431f2d0cc18b187af06bbe2e3ad571275
Michał Krawczykowski <mkrawczykowski@gmail.com>
2023-06-16 17:58:53 +0200

    styling footer

--- footer.php
+++ footer.php
@@ -1,4 +1,25 @@
     <footer class="footer">
+      <div class="container">
+        <div class="row">
+          <div class="col col--50">
+            <nav>
+              <?php
+                wp_nav_menu( array(
+                  'theme_location' => 'footer-menu',
+                  'container'      => 'div',
+                  'container_class'=> 'footer-nav',
+                  'menu_class'     => 'footer-nav__nav',
+                ) );
+              ?>
+            </nav> 
+          </div>
+          <div class="col col--50">
+            <p class="footer__company-name">
+              2023 © nazwa
+            </p>
+          </div>
+        </div>
+      </div>
       
     </footer>
 
